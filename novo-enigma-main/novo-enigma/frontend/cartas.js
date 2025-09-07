7e230bac0690dad374add893f49fd2814534727e
josewilsonx <167535569+josewilsonx@users.noreply.github.com>
2025-09-07 18:46:11 -0300

    Add files via upload

---
+++ novo-enigma-main/novo-enigma/frontend/cartas.js
@@ -0,0 +1,25 @@
+// Impede letras e símbolos no campo de código
+document.getElementById('codigo').addEventListener('input', function () {
+  this.value = this.value.replace(/\D/g, ''); // Remove tudo que não for número
+});
+
+function desbloquear() {
+  const input = document.getElementById('codigo');
+  const msg = document.getElementById('mensagem');
+  const valor = input.value.trim();
+
+  const mapa = {
+    '1847': 'enigma1.html', // Calmaria
+    '7408': 'enigma4.html', // Luto
+    '3092': 'enigma3.html', // Concentração
+    '5261': 'enigma5.html', // Ganância
+    '9630': 'enigma2.html'  // Agitação
+  };
+
+  if (mapa[valor]) {
+    window.location.href = mapa[valor];
+  } else {
+    msg.textContent = '❌ Código inválido. Tente novamente.';
+    msg.className = 'mensagem bad';
+  }
+}
